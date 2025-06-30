efc4863d5f97bc68e554c424d3caf5d08d4483f7
Pierrick Rancoeur <pierrick@srpweb.fr>
2025-06-30 18:41:20 +0200

    feat: modif header + favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="description" content="L'association Meow and Co recueille, soigne et fait adopter des chats trouvés ou abandonnés pour leur offrir une seconde chance. Nous agissons dans les départements de Vendée (85), Charentes-Maritime (17) et Deux-Sèvres (79).">
+    <meta name="keywords" content="chats, chatons, association, refuge">
+    <meta name="author" content="Association Meow and Co">
+
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,7 +18,11 @@
 
     <!-- Title -->
     <title>{{ ($title ?? '') . ' | ' }}Association Meow and Co</title>
-    {{--<link rel="icon" type="image/svg" href="assets/images/favicon.svg">--}}
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 <body>
 
