16d737a06ced17b2e1176140bf7b29de4cc57f24
Pierrick Rancoeur <pierrick@srpweb.fr>
2025-06-30 19:48:23 +0200

    fix: filament required cv

--- app/Filament/Resources/ChatResource.php
+++ app/Filament/Resources/ChatResource.php
@@ -42,7 +42,7 @@ class ChatResource extends Resource
                 TextInput::make('nom')
                     ->required(),
 
-                SpatieMediaLibraryFileUpload::make('cv')->collection('cv'),
+                SpatieMediaLibraryFileUpload::make('cv')->collection('cv')->required(),
 
                 Select::make('sexe')
                 ->options([
